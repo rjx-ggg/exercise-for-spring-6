2359bcc69e8bf062af3a2f3032103c2e59b6e58f
dyed-bear <1839833172@qq.com>
2025-08-29 07:55:20 +0800

    <重修Spring之> 2025-08-29 07:54:41【创建iocxml子模块】

--- spring6/spring-first/src/main/java/com/ssll/spring6/TestUser.java
+++ spring6/spring-first/src/main/java/com/ssll/spring6/TestUser.java
@@ -1,6 +1,8 @@
 package com.ssll.spring6;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,6 +19,8 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class TestUser {
 
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
+
     @Test
     public void testUser(){
         //1.加载配置文件
@@ -26,6 +30,10 @@ public class TestUser {
         System.out.println(user);
         //3.调用方法
         user.add();
+        // 4.日志
+        logger.info("###：日志输出成功");
+        logger.warn("###：日志输出成功");
+        logger.debug("###：日志输出成功");
     }
 
     /**
@@ -40,6 +48,12 @@ public class TestUser {
         user.add();
 
         // Bean存放的位置：DefaultListableBeanFactory类 的 beanDefinitionMap 字段
+
+        // BeanDefinitionReader：
+        // 实例化到 IOC, BeanFactory 工程 + 反射
+
+
+
 
     }
 }
---
+++ spring6/spring6-ioc-xml/src/main/java/com/ssll/iocxml/User.java
@@ -0,0 +1,18 @@
+package com.ssll.iocxml;
+
+/**
+ * ClassName: User
+ * Description:
+ *
+ * @Author 染染熊
+ * @Version 1.0
+ * @Create 2025/8/29 7:52
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public void run() {
+        System.out.println("user run...");
+    }
+}
